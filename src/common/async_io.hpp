688aca171a2c0fe6ef3d662d08dea450d55a8fa7
PragmaTwice <i@twice.moe>
2022-04-04 15:11:11 +0800

    fix async io

--- src/common/async_io.hpp
+++ src/common/async_io.hpp
@@ -17,9 +17,9 @@
 #define PROXINJECT_COMMON_ASYNC_IO
 
 #include <asio.hpp>
+#include <iostream>
 #include <protopuf/message.h>
 #include <span>
-#include <iostream>
 
 namespace ip = asio::ip;
 using tcp = asio::ip::tcp;
@@ -31,8 +31,7 @@ asio::awaitable<Message> async_read_message(Stream &s) {
                             asio::use_awaitable);
 
   std::vector<std::byte> buf(len);
-  co_await asio::async_read(s, asio::buffer(buf, len),
-                            asio::use_awaitable);
+  co_await asio::async_read(s, asio::buffer(buf, len), asio::use_awaitable);
 
   auto [msg, remains] =
       pp::message_coder<Message>::decode(std::span(buf.begin(), buf.end()));
@@ -48,10 +47,10 @@ asio::awaitable<void> async_write_message(Stream &s, const Message &msg) {
 
   co_await asio::async_write(s, asio::buffer(&len, sizeof(len)),
                              asio::use_awaitable);
-  co_await asio::async_write(s, asio::buffer(buf, len),
-                             asio::use_awaitable);
+  co_await asio::async_write(s, asio::buffer(buf, len), asio::use_awaitable);
 }
 
-static inline auto proxinject_endpoint = tcp::endpoint(ip::address::from_string("127.0.0.1"), 33321);
+static inline const auto proxinject_endpoint =
+    tcp::endpoint(ip::address::from_string("127.0.0.1"), 33321);
 
 #endif
